d72267b3cd3d7fefc075bb687cc10df782188db1
danglol240 <113696574+danglol240@users.noreply.github.com>
2024-12-14 12:46:54 +0700

    add requirement

--- homepage.py
+++ homepage.py
@@ -8,5 +8,5 @@ st.set_page_config(
 st.header("XIN CHÀO :wave:",divider="blue")
 st.write("Dưới đây là bài làm :red[Project-1] của 1 sinh viên Bách Khoa :flag-vn:")
 st.subheader("Chủ đề :art:")
-st.write("Sử dụng solution mediapipe của google và ứng dụng vẽ bằng Hands Gestures")
+st.write("Sử dụng solution :blue[mediapipe] của google và ứng dụng vẽ bằng :blue[Hands Gestures]")
 st.sidebar.success("Select the app")
