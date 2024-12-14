a84aace57ae5dfeb83c3253ca2b1a4a060527506
danglol240 <113696574+danglol240@users.noreply.github.com>
2024-12-14 12:36:48 +0700

    changes

--- homepage.py
+++ homepage.py
@@ -6,5 +6,7 @@ st.set_page_config(
 )
 
 st.header("XIN CHÀO :wave:",divider="blue")
-st.write("Dưới đây là bài làm: :red[Project-1] của 1 sinh viên Bách Khoa")
+st.write("Dưới đây là bài làm :red[Project-1] của 1 sinh viên Bách Khoa :flag-vn:")
+st.subheader("Chủ đề :art:")
+st.write("Sử dụng solution mediapipe của google và ứng dụng vẽ bằng Hands Gestures")
 st.sidebar.success("Select the app")
