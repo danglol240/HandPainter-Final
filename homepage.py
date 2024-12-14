70a1c34e1e2d32dca7de2001915c2bcb1c83ed42
danglol240 <113696574+danglol240@users.noreply.github.com>
2024-12-14 12:03:03 +0700

    change homepage

--- homepage.py
+++ homepage.py
@@ -5,5 +5,15 @@ st.set_page_config(
     page_icon=":notebook_with_decorative_cover:",
 )
 
-st.header("XIN CHÀO")
+st.header("XIN CHÀO :wave:",divider="blue")
+#st.write("Dưới đây là bài làm Project-1 của 1 sinh viên Bách Khoa")
+st.markdown("""
+<style>
+.big-font {
+    font-size:300px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">Hello World !!</p>', unsafe_allow_html=True)
 st.sidebar.success("Select the app")
